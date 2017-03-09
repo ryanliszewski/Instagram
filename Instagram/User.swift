0670510ccfee8ad26bd0f21e5b9d50f4e8a6b671
Ryanliszewski <ryanliszewski@gmail.com>
2017-03-09 11:48:06 -0800

    Added User model class

---
+++ Instagram/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  Instagram
+//
+//  Created by Ryan Liszewski on 3/9/17.
+//  Copyright © 2017 Smiley. All rights reserved.
+//
+
+import UIKit
+
+class User: NSObject {
+
+}
